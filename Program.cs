bbe39b697d9777492e5ed94af07725ccc32d5313
benard <dwlbenard@gmail.com>
2021-08-18 08:46:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NameDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print my name to console
+            Console.WriteLine("Adewole Benard Onimisi");
+
+            Console.ReadLine();
+        }
+    }
+}
